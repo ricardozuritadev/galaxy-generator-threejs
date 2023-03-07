978378cd96efb40d03390bf39eabc4c01110f77d
rizuba <r.zurita@cleverpy.com>
2023-03-07 16:09:25 +0100

    Delete test cube

--- src/script.js
+++ src/script.js
@@ -15,15 +15,6 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 /**
- * Test cube
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
-
-/**
  * Sizes
  */
 const sizes = {
